152ed3d0be1c12056ab6e5f653702e2d07ee634d
raksjain31 <rakshitjain610@gmail.com>
2025-08-13 07:35:36 +0530

    change frontend connection

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8081/api/v1" : "/api/v1",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8081/api/v1" : "https://api.abugoadyouthconnect.com/api/v1",
     withCredentials: true,
 });
-
+//"/api/v1"
 export default axiosInstance
