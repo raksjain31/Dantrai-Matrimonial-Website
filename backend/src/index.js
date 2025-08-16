75c439ae6fbd2e3de423b851944958c8149bb83b
raksjain31 <rakshitjain610@gmail.com>
2025-08-16 17:50:29 +0530

    index file log

--- backend/src/index.js
+++ backend/src/index.js
@@ -55,4 +55,5 @@ app.use("/api/v1/admin", adminRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on Port ${process.env.PORT}`);
+    console.log("✅ Server starting from VPS index.js...");
 })
