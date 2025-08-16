ff7b5d4241e0288614328a01c483fc1547469afd
raksjain31 <rakshitjain610@gmail.com>
2025-08-16 09:50:52 +0530

    passwordResetExpiry date save

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -244,13 +244,13 @@ export const forgetpassword = async (req, res) => {
 
         const otp = String(crypto.randomInt(100000, 999999));
         const hashed = crypto.createHash("sha256").update(otp).digest("hex");
-        //const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
-        const cleanDate = toUTCDateOnly(expiresAt);
+        const passwordResetExpiry = new Date(Date.now() + 10 * 60 * 1000);
+        const cleanDate = toUTCDateOnly(passwordResetExpiry);
         const expiresAtISO = cleanDate.toISOString();
 
 
-        const expiresAt = new Date().now;
-        expiresAt.setMinutes(expiresAt.getMinutes() + 10);
+        //const expiresAt = new Date().now;
+        //expiresAt.setMinutes(expiresAt.getMinutes() + 10);
 
         console.log("expiresAT:", expiresAt);
 
@@ -265,7 +265,7 @@ export const forgetpassword = async (req, res) => {
             },
             data: {
                 passwordResetToken: hashed,
-                passwordResetExpiry: expiresAt,
+                passwordResetExpiry: passwordResetExpiry,
             },
         });
 
