87aa11b3ded03ea3d0f62dacc2e4b800191d12c7
raksjain31 <rakshitjain610@gmail.com>
2025-08-17 06:57:15 +0530

    login console for debugging added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -122,7 +122,7 @@ export const login = async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: "1d" }
         )
-
+        console.log("login time data:", user);
         res.cookie("jwt", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV !== "development",
